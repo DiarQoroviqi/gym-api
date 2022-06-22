026c52468f343fa57d9cdc350f41f10aad321b4c
Diar Qoroviçi <diar@CODAs-MacBook-Pro.local>
2022-06-22 15:55:54 +0200

    Code lint

--- backend/app/Http/Resources/Api/V1/ContractResource.php
+++ backend/app/Http/Resources/Api/V1/ContractResource.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Http\Resources\Api\V1;
 
-use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ContractResource extends JsonResource
--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
